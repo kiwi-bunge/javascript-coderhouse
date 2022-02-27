c537d58e2e516d1aab6eb834ab50e10090217987
Augusto Bunge <abunge7@gmail.com>
2022-02-27 18:08:01 +1100

    shopping cart logic and bugs fixed

--- javascript/add-to-cart.js
+++ javascript/add-to-cart.js
@@ -1,69 +1,236 @@
-import updateCart from "./update-cart.js";
-import { PRODUCTS } from "./stock.js";
+const openCart = document.getElementById("btnShoppingCart");
+const closeCart = document.getElementById("btnCloseCart");
+const modalContainer = document.getElementsByClassName("modalBox")[0];
+const modalCart = document.getElementsByClassName("modalShoppingCart")[0];
+
+const cartContainer = document.getElementById("cart-container");
+const productsContainer = document.getElementById("products-container");
+const cartQuantity = document.getElementById("cartQuantity");
+const totalCost = document.getElementById("totalCost");
+
+import { PRODUCTS } from "./stock.js"
 
 let shoppingCart = [];
 
-const cartContainer = document.getElementById("cart-container");
+showProducts();
 
-// shoppingCart = JSON.parse(localStorage.getItem('carrito')) || [];
+loadEventListeners();
 
-export default function addToCart(id) {
+
+
+// Shopping Cart Modal
+
+openCart.addEventListener("click", () => {
+
+    modalContainer.classList.toggle("modal-visible")
+});
+closeCart.addEventListener("click", () => {
+
+    modalContainer.classList.toggle("modal-visible")
+});
+modalCart.addEventListener('click',(e)=>{
+    e.stopPropagation()
+});
+modalContainer.addEventListener('click', ()=>{
+    carritoCerrar.click()
+});
+
+
+//Load event listeners
+
+function loadEventListeners() {
+
+    productsContainer.addEventListener("click", addToCart);
     
-    let repeated = shoppingCart.find(repeatedProduct => repeatedProduct.id == id);
+    modalCart.addEventListener("click", eliminateProduct);
+    
+    updateCart(shoppingCart);
 
-    if(repeated) {
+    document.addEventListener('DOMContentLoaded', () => {
 
-        repeated.quantity = repeated.quantity + 1;
-
-        document.getElementById(`quantity${repeated.id}`).innerHTML = `<p id=quantity${repeated.id}>Quantity:${repeated.quantity}</p>`;
-
+        shoppingCart = JSON.parse(localStorage.getItem("cart")) || [];
+        cartHTML();
         updateCart(shoppingCart);
-        syncLocalStorage();
+        
+        
+    });
 
-    } else {
+    updateCart(shoppingCart);
+};
 
-        let product = PRODUCTS.find(product => product.id == id);
 
-        shoppingCart.push(product);
+// Show products in body
 
-        product.quantity = 1;
+function showProducts() {
+    
+    PRODUCTS.forEach (product => {
+        
+        const {id, img, name, price} = product;
 
         let div = document.createElement("div");
+        div.classList.add("product");
+        div.innerHTML = `
+                        <div class="product-card" data-id=${id}>
+                            <div class="card-image">
+                                <img src= ${img}>
+                            </div>
+                            <p class= "card-title">
+                                ${name}
+                            </p>
+                            <p class= "card-price">
+                                $<span>${price}</span>
+                            </p>
+                            <button class="btnAddToCart" id="addToCartButton${id}" data-id="${id}"> Add to Cart </button>
+                        </div>
+        `;
+
+        productsContainer.appendChild(div);
+    });  
+};
+
+
+
+// Add products to cart
+
+function addToCart(e) {
+
+    e.preventDefault();
+    if (e.target.classList.contains("btnAddToCart")) {
+        const selectedProduct = e.target.parentElement.parentElement;
+        productInCart(selectedProduct);
+        Toastify({
+            text: "🛒 Product Added",
+            duration: 2000,
+            stopOnFocus: true,
+
+            style: {
+                background: "#f5e4bb",
+                color: "#83472C"
+            },
+        }).showToast();
+    };
+    updateCart(shoppingCart);
+};
+
+
+// Elminating product from cart
+
+function eliminateProduct(e) {
+    
+    if (e.target.classList.contains("eliminateProductFromCart")) {
+        // console.log(e.target.getAttribute("data-id"));
+        const productId = e.target.getAttribute("data-id");
+
+        shoppingCart = shoppingCart.filter(product => product.id !== productId);
+        Toastify({
+            text: "🛒 Product Removed",
+            duration: 2000,
+            stopOnFocus: true,
+
+            style: {
+                background: "#83472C",
+                color: "#f5e4bb"
+            },
+        }).showToast();
+
+        cartHTML();
+    };
+    updateCart(shoppingCart);
+};
+
+// Cleaning HTML
+
+function cleanHTML() {
+
+    while(cartContainer.firstChild) {
+        cartContainer.removeChild(cartContainer.firstChild);
+    };
+};
+
+
+
+//Update cart
+
+function updateCart(shoppingCart) {
+
+    cartQuantity.innerHTML = shoppingCart.reduce((acc, el) => acc + el.quantity, 0);
+    totalCost.innerHTML = shoppingCart.reduce((acc, el) => acc + (el.price * el.quantity), 0).toFixed(2);
+
+}
+
+
+// Add to cart
+
+function productInCart(product) {
+    
+    const productInfo = {
+
+        id: product.querySelector("button").getAttribute("data-id"),
+        name: product.querySelector(".card-title").textContent,
+        price: product.querySelector(".card-price span").textContent,
+        
+        quantity: 1
+    };
+
+    const repeated = shoppingCart.some (product => product.id === productInfo.id);
+
+    if (repeated) {
+
+        const repeatedProducts = shoppingCart.map (product => {
+
+            if (product.id === productInfo.id) {
+
+                product.quantity ++;
+                return product;
+            } else {
+
+                return product;
+            }
+        });
+        shoppingCart = [...repeatedProducts];
+    } else {
+
+        shoppingCart = [...shoppingCart, productInfo];
+    };
+
+    cartHTML();
+};
+
+
+// Showing Shopping Cart
+
+function cartHTML() {
+
+    cleanHTML();
+
+    shoppingCart.forEach (product => {
+
+        const {name, price, quantity, id} = product;
+        const div = document.createElement("div");
 
         div.className = "productInCart";
         div.innerHTML = `
-                        <p>${product.name}</p>
-                        <p>Price: $${product.price}</p>
-                        <p id=quantity${product.id}>Quantity: ${product.quantity}</p>
-                        <button id=eliminate${product.id} class="eliminateButton"> Delete </button>
+                    <p>${name}</p>
+                    <p>Price: $ ${price}</p>
+                    <p id=quantity${id}>Quantity: ${quantity}</p>
+                    <button class="eliminateProductFromCart" id="deleteButton${id}" data-id="${id}"> Delete </button>
         `;
 
         cartContainer.appendChild(div);
-        
-        updateCart(shoppingCart);
-        syncLocalStorage();
+        updateCart(shoppingCart)
+    });
 
-        let eliminateButton = document.getElementById(`eliminate${product.id}`);
+    syncLocalStorage();
+};
 
-        eliminateButton.addEventListener("click", () => {
 
-            eliminateButton.parentElement.remove();
-            shoppingCart = shoppingCart.filter(el => el.id != product.id);
-            updateCart(shoppingCart);
-            syncLocalStorage();
-        });
-        
-    };
-}
-
+//  Setting Local Storage
 function syncLocalStorage() {
     localStorage.setItem("cart", JSON.stringify(shoppingCart));
-    console.log(shoppingCart)
-}
+};
 
-// function loadLocalStorage() {
-//     shoppingCart = JSON.parse(localStorage.getItem("cart")) || [];
-// }
+
+
 
 
 
